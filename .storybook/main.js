7f8b30e47b792721df524963bd55d5cd505da4cc
halilatilla <atillahalil0@gmail.com>
2021-04-01 17:28:22 +0300

    Add storybook i18n , css modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../components/**/*.stories.js'],
   addons: [
     '@storybook/addon-essentials',
+    'storybook-css-modules-preset',
     {
       name: '@storybook/addon-postcss',
       options: {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,16 @@
+import I18nProvider from 'next-translate/I18nProvider'
+
+import common from '../locales/en/common.json'
 import '../styles/globals.css'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
 }
+
+export const decorators = [
+  Story => (
+    <I18nProvider lang="en" namespaces={{ common }}>
+      <Story />
+    </I18nProvider>
+  )
+]
